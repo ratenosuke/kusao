582b702ca448aa7b0dbc21d5efd006238bf6df91
小泉心之介 <shin200020529@gmail.com>
2015-04-24 20:26:23 +0900

    はしお臭いよ

--- hasiokusasi/ViewController.swift
+++ hasiokusasi/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var number: Int = 0
+    @IBOutlet var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,6 +21,46 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+
+
+    @IBAction func plus() {
+        number=number + 1
+        label.text = String(number)
+        
+    }
+    
+    @IBAction func mainasu() {
+        number=number - 1
+        label.text = String(number)
+        
+    }
+    
+    @IBAction func kakeru() {
+        number=number*2
+        label.text = String(number)
+        
+    }
+
+    @IBAction func waru() {
+        number=number/2
+        label.text = String(number)
+        
+    }
+    
+    
+    @IBAction func kuria() {
+        number=0
+        label.text = String(number)
+        
+    }
+
+    @IBAction func kusai() {
+        if number >= 5000 {
+            label.text = String("臭すぎて死ぬ")
+        }
+    }
+
+
 
 
 }
